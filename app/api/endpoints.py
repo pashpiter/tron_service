f3ca1d2a6e03064cb67851cff30c6197e0c8976b
Pavel Drovnin <pavel98rus@gmail.com>
2025-04-17 16:05:23 +0300

    latest_wallet_query endpoint

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -2,25 +2,29 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database.connection import get_session
-from schemas.wallet import (WalletResponse, LatestWalletQueryListResponse,
+from database.crud import get_latest_wallet_query
+from schemas.wallet import (WalletResponse, LatestWalletQueryList,
                             WalletRequest)
 
 
-router = APIRouter(prefix='/')
+router = APIRouter()
 
 
-@router.get('/', response_model=LatestWalletQueryListResponse)
+@router.get('/', response_model=LatestWalletQueryList)
 async def latest_wallet_query(
     offset: int = 0,
     limit: int = 10,
     session: AsyncSession = Depends(get_session)
 ):
-    pass
+    query = await get_latest_wallet_query(
+        session, offset, limit
+    )
+    return query
 
 
 @router.post('/', response_model=WalletResponse)
 async def get_info_about_adress(
-    adress: WalletRequest,
+    address: WalletRequest,
     session: AsyncSession = Depends(get_session)
 ):
     pass
