8029fadaa73a16bbb2997dc020813d6b05707e73
Pavel Drovnin <pavel98rus@gmail.com>
2025-04-17 13:49:14 +0300

    schemas fix

--- app/schemas/wallet.py
+++ app/schemas/wallet.py
@@ -1,12 +1,32 @@
+from datetime import datetime
+from typing import List
 from pydantic import BaseModel
+
+
+class WalletBase(BaseModel):
+    wallet_address: str
+    trx_balance: float
+    bandwidth: int
+    energy: int
+
+
+class Wallet(WalletBase):
+    id: int
+    timestamp: datetime
+
+    class Config:
+        orm_mode = True
 
 
 class WalletRequest(BaseModel):
     address: str
 
 
-class WalletResponse(BaseModel):
-    address: str
-    trx_balance: float
-    bandwidth: int
-    energy: int
+class WalletResponse(WalletBase):
+    pass
+
+
+class LatestWalletQueryListResponse(BaseModel):
+    items: List[WalletBase]
+    limit: int
+    offset: int
