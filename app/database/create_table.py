28c3518c00fc76ddbfe7002b7e540a5cb773cdb5
Pavel Drovnin <pavel98rus@gmail.com>
2025-04-17 15:49:49 +0300

    create table at startup

---
+++ app/database/create_table.py
@@ -0,0 +1,9 @@
+from database.connection import async_engine
+
+from models.wallet import Base
+
+
+async def create_table() -> None:
+    '''Создание таблиц и добавление админа'''
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from core.config import settings
 from api.endpoints import router
+from database.create_table import create_table
 
 
-app = FastAPI(debug=settings.app.debug)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_table()
+    yield
+
+app = FastAPI(debug=settings.app.debug, lifespan=lifespan)
 app.include_router(router)
