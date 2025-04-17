927e190abb8866468651d547436c7eb7588aa4cb
Pavel Drovnin <pavel98rus@gmail.com>
2025-04-17 16:05:46 +0300

    get_latest_wallet_query crud

--- app/database/crud.py
+++ app/database/crud.py
@@ -2,19 +2,30 @@ from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 
-from models.wallet import Wallet
+from models.wallet import WalletQueryDB
+from schemas.wallet import WalletQuery, LatestWalletQueryList
 
 
 async def get_latest_wallet_query(
         session: AsyncSession,
         offset: int,
         limit: int
-) -> list[Wallet]:
-    pass
+) -> list[WalletQuery]:
+    stmt = (
+        select(WalletQueryDB)
+        .offset(offset)
+        .limit(limit)
+        .order_by(WalletQueryDB.id.desc())
+    )
+    result: Result = await session.execute(stmt)
+    items = [
+        WalletQuery.model_validate(item) for item in result.scalars().all()
+    ]
+    return LatestWalletQueryList(items=items)
 
 
 async def create_wallet_request(
         session: AsyncSession,
-        wallet: Wallet
-) -> Wallet:
+        wallet: WalletQuery
+) -> WalletQuery:
     pass
