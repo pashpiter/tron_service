5666faa9a0fca005eb498f8162bbcb432d6b0cd5
Pavel Drovnin <pavel98rus@gmail.com>
2025-04-17 13:49:32 +0300

    endpoints funcs

---
+++ app/api/endpoints.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from database.connection import get_session
+from schemas.wallet import (WalletResponse, LatestWalletQueryListResponse,
+                            WalletRequest)
+
+
+router = APIRouter('/')
+
+
+@router.get('/', response_model=LatestWalletQueryListResponse)
+async def latest_wallet_query(
+    offset: int = 0,
+    limit: int = 10,
+    session: AsyncSession = Depends(get_session)
+):
+    pass
+
+
+@router.post('/', response_model=WalletResponse)
+async def get_info_about_adress(
+    adress: WalletRequest,
+    session: AsyncSession = Depends(get_session)
+):
+    pass
