aa487233444d455ded5db8843dd08f3472ef144c
Pavel Drovnin <pavel98rus@gmail.com>
2025-04-20 12:18:17 +0300

    tron service get_wallet_info

---
+++ app/core/services.py
@@ -0,0 +1,41 @@
+from tronpy import Tron
+
+from core.config import settings
+
+
+class TronService:
+    def __init__(self):
+        self.client = Tron(network=settings.tron.network)
+
+    async def get_wallet_info(
+            self, address: str
+    ) -> dict[str, str | int | float]:
+        if not self.client.is_address(address):
+            raise ValueError('Invalid TRON address')
+        account_resources = self.client.get_account_resource(address)
+        trx_balance = float(self.client.get_account_balance(address))
+        bandwidth = max(
+            0,
+            account_resources.get(
+                'freeNetLimit', 0
+            ) - account_resources.get(
+                'freeNetUsed', 0
+            )
+        )
+        energy = max(
+            0,
+            account_resources.get(
+                'EnergyLimit', 0
+            ) - account_resources.get(
+                'EnergyUsed', 0
+            )
+        )
+        return {
+            'address': address,
+            'trx_balance': trx_balance,
+            'bandwidth': bandwidth,
+            'energy': energy
+        }
+
+
+tron = TronService()
